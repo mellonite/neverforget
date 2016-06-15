a93df087ab24a3f0c1447b3761b6c9dc830bfdd1
mel <melgriffin@protonmail.com>
2016-06-15 10:07:08 -0400

    Checks that user input is correct and handles incorrect input.

--- neverforget.py
+++ neverforget.py
@@ -1,10 +1,8 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram import Emoji, ForceReply, ReplyKeyboardMarkup, KeyboardButton
+from telegram import ForceReply, ReplyKeyboardMarkup, KeyboardButton, ParseMode
 import logging
 import time
 import python3pickledb as pickledb
-
-YES, NO = (Emoji.THUMBS_UP_SIGN, Emoji.THUMBS_DOWN_SIGN)
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -16,9 +14,10 @@ logger = logging.getLogger(__name__)
 db = pickledb.load('bot.db', True)
 
 helpText = 'I will send you messages about the time. ' \
-           '\n/add your own reminders' \
-           '\nUse syntax: /add hh:mm message' \
-           '\nExample: "/add 11:11 wish" will send a /wish reminder at 11:11'
+           '\n\n/add your own reminders' \
+           '\nUse syntax: /add HH:MM message' \
+           '\nI run on a 24-hour clock!' \
+           '\n\n/remove reminders you have added'
 
 # initialize the database
 if not db.get('alerts') :
@@ -48,11 +47,15 @@ def remove_value(bot, update):
     # Since the handler will also be called on messages, we need to check if
     # the message is actually a command
     if chat_state == MENU and text[0] == '/':
-        state[chat_id] = AWAIT_INPUT  # set the state
-
-        context[chat_id] = user_id  # save the user id to context
-        msg = removeMsg(bot, update)
-        bot.sendMessage(chat_id, text=msg, reply_markup=ForceReply())
+        alerts = db.get('alerts')
+        # check that he has alerts
+        if chat_id not in alerts or not alerts[chat_id].keys():
+            bot.sendMessage(chat_id, text='You have no reminders! See /help for more info.')
+        else:
+            state[chat_id] = AWAIT_INPUT  # set the state
+            context[chat_id] = user_id  # save the user id to context
+            msg = removeMsg(bot, update)
+            bot.sendMessage(chat_id, text=msg, reply_markup=ForceReply())
 
     # If we are waiting for input and the right user answered
     elif chat_state == AWAIT_INPUT and chat_context == user_id:
@@ -61,10 +64,11 @@ def remove_value(bot, update):
         # Save the user id and the answer to context
         context[chat_id] = (user_id, update.message.text)
         reply_markup = ReplyKeyboardMarkup(
-            [[KeyboardButton(YES), KeyboardButton(NO)]],
+            [['Yes', 'No']],
             one_time_keyboard=True)
         bot.sendMessage(chat_id,
-                        text="Are you sure you would like to remove %s?" % update.message.text,
+                        text="Are you sure you would like to remove *%s*?" % update.message.text,
+                        parse_mode=ParseMode.MARKDOWN,
                         reply_markup=reply_markup)
 
     # If we are waiting for confirmation and the right user answered
@@ -72,13 +76,16 @@ def remove_value(bot, update):
         del state[chat_id]
 
         alerts = db.get('alerts')
-        if text == YES:
-            bot.sendMessage(chat_id, text="I will no longer remind you to %s." % context[chat_id])
-            alerts[chat_id].pop(context[chat_id][1:])
+        if text == 'Yes':
+            bot.sendMessage(chat_id,
+                            text="I will no longer remind you to *%s*." % str(context[chat_id][1]),
+                            parse_mode=ParseMode.MARKDOWN)
+            alerts[chat_id].pop(context[chat_id][1])
             db.set('alerts', alerts)
         else:
             bot.sendMessage(chat_id,
-                            text="I will keep reminding you to %s.\, then" % alerts[chat_id][chat_context[1:]])
+                            text="I will keep reminding you to *%s*, then." % str(context[chat_id][1]),
+                            parse_mode=ParseMode.MARKDOWN)
 
         del context[chat_id]
 
@@ -86,15 +93,18 @@ def remove_value(bot, update):
 
 
 def removeMsg(bot, update):
-    message = 'Alright, which of these reminders would you like to remove?'
+    chat_id = str(update.message.chat_id)
+    message = 'Alright, which of these reminders would you like to remove?\n'
 
     # get all of the reminders saved for this chat
     alerts = db.get('alerts')
-    reminders = ['/' + k for k in alerts[str(update.message.chat_id)].keys()]
+
+    reminders = [k for k in alerts[chat_id].keys()]
 
     for r in reminders:
         message += '\n' + r
 
+    message += '\n\nIf you\'ve changed your mind, send /cancel.'
     return message
 
 # Handler for the /cancel command.
@@ -109,20 +119,52 @@ def cancel(bot, update):
 def help(bot, update):
     bot.sendMessage(update.message.chat_id, text=helpText)
 
+addError = 'Please use the format: /add HH:MM message.' \
+           '\nExample: /add 11:11 wish' \
+           '\nI run on a 24-hour clock!'
+
+existingAlert = 'I already remind you to *%s*!' \
+                '\nPlease pick a new name for your reminder or /remove the existing one.'
+
 def add(bot, update):
     chat_id = str(update.message.chat_id)
     splitMsg = update.message.text.split(' ')
+
+    #Check the formatting
+    if len(splitMsg) != 3:
+        bot.sendMessage(update.message.chat_id, text=addError)
+        return
+
     alertTime = splitMsg[1]
+
+    checkTime = alertTime.split(':')
+
+    # The time must consist only of two digits, one with no more than 2 digits
+    # and one with exactly two digits
+    if len(checkTime) != 2 or not checkTime[0].isdigit() or not checkTime[1].isdigit() \
+       or len(checkTime[1]) != 2 or len(checkTime[0]) > 2:
+        bot.sendMessage(update.message.chat_id, text=addError)
+        return
+
     alertMsg = splitMsg[2]
 
     alerts = db.get('alerts')
+
     # if the chat_id does not yet exist in the database, add it
     if chat_id not in alerts:
         alerts[chat_id] = {}
-    alerts[chat_id][alertMsg] = alertTime
-    db.set('alerts', alerts)
 
-    bot.sendMessage(update.message.chat_id, text='OK, at ' + alertTime + ' I will remind you to ' + alertMsg + ' every day')
+    # Check that the alert doesn't already exist
+    if alertMsg in alerts[chat_id]:
+        bot.sendMessage(update.message.chat_id,
+                        text=existingAlert % alertMsg,
+                        parse_mode=ParseMode.MARKDOWN)
+    else:
+        alerts[chat_id][alertMsg] = alertTime
+        bot.sendMessage(update.message.chat_id,
+                        text='OK, at *%s* I will remind you to *%s* every day' % (alertTime, alertMsg),
+                        parse_mode=ParseMode.MARKDOWN)
+    db.set('alerts', alerts)
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
